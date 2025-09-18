50e6df57786485d345a6c55f05bebc75683cdb31
ArnLundberg1 <146753285+ArnLundberg1@users.noreply.github.com>
2025-09-18 17:36:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -24,19 +24,5 @@ window.APP_CONFIG = {
     resrobot: {
     apiKey: "e5e2089c-3a67-4d2f-a957-b86f06b82436",  // Byt ut mot din Trafiklab-nyckel
     baseUrl: "https://api.resrobot.se/v2.1/trip",
-    geometry: true,
-      // Färgmedel: Buses, Metro, Tram, Ferries, Local trains, High speed trains, Regional trains
-      products: 502
-    },
-    products: {
-      AIR: 1,
-      HIGH_SPEED_TRAIN: 2,
-      REGIONAL_TRAIN: 4,
-      LOCAL_TRAIN: 16,
-      METRO: 32,
-      TRAM: 64,
-      BUS: 128,
-      FERRY: 256,
-      TAXI: 512
     }
 };
